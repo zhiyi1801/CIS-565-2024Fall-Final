0c2f703c83c6d14165a9f6e6509bb51fd58eb010
jichumao <jichumao@qq.com>
2024-11-17 18:44:30 -0500

    Fix glsl grid build logic

--- shaders/HashBuildStructure.glsl
+++ shaders/HashBuildStructure.glsl
@@ -1,12 +1,8 @@
-// TODO: Rewrite RWByteAddressBuffer jenkinsHash pcg32
-
 
 #ifndef HASH_BUILD_STRUCTURE_GLSL
 #define HASH_BUILD_STRUCTURE_GLSL
 
-
 #include "host_device.h"
-
 //struct HashAppendData
 //{
 //    uint isValid;
@@ -28,95 +24,118 @@
 //    float minCellSize = 0.0f;   
 //};
 
-
-uint pcg32(uint input)
+// TODO: Rewrite RWByteAddressBuffer jenkinsHash pcg32
+uint pcg32(uint inp)
 {
-    uint state = input * 747796405u + 2891336453u;
+    uint state = inp * 747796405u + 2891336453u;
     uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
     return (word >> 22u) ^ word;
 }
 
-uint BinaryNorm(float3 norm)
+uint BinaryNorm(vec3 norm)
 {
-    uint a = norm.x > 0.f ? 1 : 0;
-    uint b = norm.y > 0.f ? 1 : 0;
-    uint c = norm.z > 0.f ? 1 : 0;
-    return a * 100 + b * 10 + c;
-    //uint3 biNorm = floor((norm + 1.f) * 1.5f);
-    //return biNorm.x << 4 | biNorm.y << 2 | biNorm.z;
+    uint a = norm.x > 0.0 ? 1u : 0u;
+    uint b = norm.y > 0.0 ? 1u : 0u;
+    uint c = norm.z > 0.0 ? 1u : 0u;
+    return a * 100u + b * 10u + c;
 }
 
-
-float CalculateCellSize(float3 pos, float3 cameraPos, GIParameter params)
+uint jenkinsHash(uint a)
 {
-    float cellSizeStep = length(pos - cameraPos) * tan(120 * params.fov * max(1.0 / params.frameDim.y, params.frameDim.y / float((params.frameDim.x * params.frameDim.x))));
-    int logStep = floor(log2(cellSizeStep / params.minCellSize));
-   
-    return params.minCellSize * max(0.12f, exp2(logStep));
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return uint(a);
 }
 
-int FindOrInsertCell(float3 pos, float3 norm, float cellSize, GIParameter params, RWByteAddressBuffer checkSumBuffer)
+float CalculateCellSize(vec3 pos, vec3 cameraPos, GIParameter params)
 {
-    uint3 p = uint3(floor((pos - params.sceneBBMin) / cellSize));
+    float cellSizeStep = length(pos - cameraPos) * tan(120.0 * params.fov * max(1.0 / float(params.frameDim.y), float(params.frameDim.y) / float(params.frameDim.x * params.frameDim.x)));
+    int logStep = int(floor(log2(cellSizeStep / params.minCellSize)));
 
-    //uint normprint = params._pad > 0 ? BinaryNorm(norm) : 0u;
+    return params.minCellSize * max(0.12, exp2(float(logStep)));
+}
+
+// RWByteAddressBuffer in slang, but checkSumBuffer in GLSL stucture(should be bind in layout)
+int FindOrInsertCell(vec3 pos, vec3 norm, float cellSize, GIParameter params)
+{
+    uvec3 p = uvec3(
+        uint(floor((pos - params.sceneBBMin).x / cellSize)),
+        uint(floor((pos - params.sceneBBMin).y / cellSize)),
+        uint(floor((pos - params.sceneBBMin).z / cellSize))
+    );
+
     uint normprint = BinaryNorm(norm);
 
-    uint cellIndex = pcg32(normprint + pcg32(cellSize + pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000;
-    uint checkSum = max(jenkinsHash(normprint + jenkinsHash(cellSize+ jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x))))), 1);
+    uint cellIndex = pcg32(normprint + pcg32(uint(cellSize)+pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000u;
+    uint checkSum = max(uint(jenkinsHash(normprint + jenkinsHash(uint(cellSize)+jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x)))))), 1u);
 
-    for (uint i = 0; i < 32; i++)
+    for (uint i = 0u; i < 32u; i++)
     {
-        uint idx = cellIndex * 32 + i;
-        uint checkSumPre; 
+        uint idx = cellIndex * 32u + i;
+        uint expected = 0u;
+        uint desired = checkSum;
+        uint checkSumPre = atomicCompSwap(checkSumBuffer[idx], expected, desired);
+        //uint checkSumPre = atomicCompSwap(checkSumBuffer.checkSumBufferData[idx], expected, desired);
 
-        checkSumBuffer.InterlockedCompareExchange(idx, 0, checkSum, checkSumPre);
-        if (checkSumPre == 0 || checkSumPre == checkSum)
-            return idx;
+        if (checkSumPre == 0u || checkSumPre == checkSum)
+            return int(idx);
     }
 
     return -1;
 }
 
-int FindCell(float3 pos, float3 jitteredPos, float3 norm, float cellSize, GIParameter params, RWByteAddressBuffer checkSumBuffer, inout SampleGenerator sg)
+int FindCell(vec3 pos, vec3 jitteredPos, vec3 norm, float cellSize, GIParameter params)
 {
-    uint3 p = uint3(floor((pos - params.sceneBBMin) / cellSize));
+    uvec3 p = uvec3(
+        uint(floor((pos - params.sceneBBMin).x / cellSize)),
+        uint(floor((pos - params.sceneBBMin).y / cellSize)),
+        uint(floor((pos - params.sceneBBMin).z / cellSize))
+    );
 
-    //uint normprint = params._pad > 0 ? BinaryNorm(norm) : 0u;
     uint normprint = BinaryNorm(norm);
-    uint cellIndex = pcg32(normprint + pcg32(cellSize + pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000;
-    uint checkSum = max(jenkinsHash(normprint + jenkinsHash(cellSize + jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x))))), 1);
+    uint cellIndex = pcg32(normprint + pcg32(uint(cellSize)+pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000u;
+    uint checkSum = max(uint(jenkinsHash(normprint + jenkinsHash(uint(cellSize)+jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x)))))), 1u);
 
-
-    for (uint i = 0; i < 32; i++)
+    for (uint i = 0u; i < 32u; i++)
     {
-        uint idx = cellIndex * 32 + i;
+        uint idx = cellIndex * 32u + i;
 
-        if (checkSumBuffer.Load(idx) == checkSum)
-            return idx;
+        // * rewrite the structure
+        if (checkSumBuffer[idx] == checkSum)
+            return int(idx);
     }
 
     return -1;
 }
 
-int FindCell(float3 jitteredPos, float3 norm, float cellSize, GIParameter params, ByteAddressBuffer checkSumBuffer)
-{ // + float3(1, 1, 1) * 0.001f;
-    //+ (sampleNext3D(sg) * 2.0f - 1.0f) * 0.001f; // * cellSize;
-    //-params.sceneBBMin;
-    uint3 p = uint3(floor((jitteredPos - params.sceneBBMin)/ cellSize));
+
+int FindCell(vec3 jitteredPos, vec3 norm, float cellSize, GIParameter params)
+{
+
+    uvec3 p = uvec3(
+        uint(floor((jitteredPos - params.sceneBBMin).x / cellSize)),
+        uint(floor((jitteredPos - params.sceneBBMin).y / cellSize)),
+        uint(floor((jitteredPos - params.sceneBBMin).z / cellSize))
+    );
 
     uint normprint = BinaryNorm(norm);
 
-    uint cellIndex = pcg32(normprint + pcg32(cellSize + pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000;
-    uint checkSum = max(jenkinsHash(normprint + jenkinsHash(cellSize + jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x))))), 1);
+    uint cellIndex = pcg32(normprint + pcg32(uint(cellSize)+pcg32(p.z + pcg32(p.y + pcg32(p.x))))) % 100000u;
+    uint checkSum = max(uint(jenkinsHash(normprint + jenkinsHash(uint(cellSize)+jenkinsHash(p.z + jenkinsHash(p.y + jenkinsHash(p.x)))))), 1u);
 
-    for (uint i = 0; i < 32; i++)
+    for (uint i = 0u; i < 32u; i++)
     {
-        uint idx = cellIndex * 32 + i;
-
-        if (checkSumBuffer.Load(idx) == checkSum)
-            return idx;
+        uint idx = cellIndex * 32u + i;
+        // rewrite structure
+        if (checkSumBuffer[idx] == checkSum)
+            return int(idx);
     }
 
     return -1;
 }
+
+#endif  // HASH_BUILD_STRUCTURE_GLSL
